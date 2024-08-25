46032134055b4cb4bfc0fda74b32103c9cba287f
HuynhThiMyDuyen01 <hmyduyen47@gmail.com>
2024-08-25 19:31:47 +0700

    Add files via upload

---
+++ slide18.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 19:22:51 2024
+
+@author: MY DUYEN
+"""
+
+import random
+start = 3 
+stop = 10
+step = 5 
+random_number = random.randrange(start, stop, step)
+print(random_number)
+
+
+
+
+import random
+x = 3.0
+y = 10.0
+random_float = random.uniform(x, y)
+print(random_float)
---
+++ slide7.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 19:11:04 2024
+
+@author: MY DUYEN
+"""
+
+a=10
+b=3
+print("a+b =",a+b)
+print("a-b =",a-b)
+print("a*b =",a*b)
+print("a/b =",a/b)
+print("a%b =",a%b)
+print("a//b =",a//b)
+x=3
+print("a^x = ",a**x)
